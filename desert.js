9006d3183a89d2fae6418857c3739b67530f5cbd
Jevgenij <35303934+jefomi@users.noreply.github.com>
2022-12-15 18:41:32 +0200

    Update desert.js

--- desert.js
+++ desert.js
@@ -1,1 +1,1 @@
-desert () => {var myFunction1 = () => {return '*** 1 ***';};var myFunction2 = () => {let func1Result = myFunction1();return '*** 2 ***:' + func1Result;};var myFunction3 = () => {return Dat();};return {myPackage: {myFunction1,myFunction2,myFunction3}};};
+desert () => {var myFunction1 = () => {return '*** 1 ***';};var myFunction2 = () => {let func1Result = myFunction1();return '*** 2 ***:' + func1Result;};var myFunction3 = () => {return Dat();};return {myPackage: {myFunction1,myFunction2,myFunction3}};}
